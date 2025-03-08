c7c865b528d81a002cb52337089edfc1dddf104a
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2025-03-08 18:54:15 +0900

    타입 정리

--- book-page-router/src/pages/_app.tsx
+++ book-page-router/src/pages/_app.tsx
@@ -1,19 +1,23 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import type { NextPage } from "next";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactNode) => React.ReactNode;
+}
 
 import GlobalLayout from "@/components/global-layout";
+import { AppProps } from "next/app";
 
 // react의 app 컴포넌트 역할 - root 의 역할
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps & {
+  Component: NextPageWithLayout;
+}) {
   // 현재 페이지의 역할을 할 Component prop
   // pageProps는 현재 페이지의 속성값
   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
-  const router = useRouter();
-
-  // mount 될 때 한번만 실행해서 어떤 페이지를 prefetch 할지 결정
+  
+  // mount 될 때 한번만 실행해서 어떤 페이지를 prefetch 할지 결정할 수 있음
+  // const router = useRouter();
   // useEffect(() => {
   //   router.prefetch("/test");
   // }, []);
