ee12d2d33821f37d96591be4dbc85e3242234740
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2026-07-06 23:15:10 +0900

    궁금한거 계속 복습

--- book-page-router/src/components/global-layout.tsx
+++ book-page-router/src/components/global-layout.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import styles from '@/components/global-layout.module.css';
+import Link from "next/link";
+import styles from "@/components/global-layout.module.css";
 
 export default function GlobalLayout({
   children,
@@ -9,7 +9,13 @@ export default function GlobalLayout({
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <Link href="/">📚 JAY BOOKS</Link>
+        {/* prefetch: 서버 왕복 없이, URL만 바꾸고 필요한 페이지 청크를 받아 화면 교체 */}
+        <Link href="/" prefetch={false}>
+          {/* false로 해서 명시적으로 프리패칭을 막을 수 있음 (기본값 true) */}
+          {/* 청크(코드)와 데이터를 구분해야 한다. 해당 페이지의 "코드"를 미리 받는 타이밍에만 영향을 주고, 데이터는 매번 새로 가져온다. */}
+          {/* 한번 받은 이후에는 캐시된다. 보통 첫 진입 페이지니까 false로 해도 괜찮다. */}
+          📚 JAY BOOKS
+        </Link>
       </header>
       <main>{children}</main>
       <footer className={styles.footer}>제작 @leejaypower</footer>
--- book-page-router/src/pages/index.tsx
+++ book-page-router/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import SearchableLayout from '@/components/searchable-layout';
-import styles from './index.module.css';
+import SearchableLayout from "@/components/searchable-layout";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
@@ -13,6 +13,8 @@ export default function Home() {
 // 1️⃣ 페이지별로 다른 레이아웃 적용 가능
 // 2️⃣ 공통 레이아웃을 유지하면서 특정 페이지만 다르게 설정 가능
 // 3️⃣ Next.js의 getServerSideProps와 함께 사용 가능
+// ⚠️ 이런 패턴을 사용안하고 그냥 각 페이지에 레이아웃 컴포넌트를 불러와서 조합하게 되면, 페이지 변경시 새로 마운트되므로
+// 여러 페이지가 같은 레이아웃 인스턴스를 공유하며 상태를 유지할 수 없음 (이건 spa도 마찬가지 - 다만 react router로 유지하도록 설정할 수 있음)
 Home.getLayout = (page: React.ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>;
 };
