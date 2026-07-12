992f7b784f0568e6785ff8a0326e9cab7af10e48
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2026-07-12 23:20:02 +0900

    async 서버 컴포넌트

--- book-app-router/src/app/(with-searchbar)/page.tsx
+++ book-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,22 +1,51 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+  return (
+    <section>
+      <h3>등록된 모든 도서</h3>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
+
+async function RecoBooks() {
+  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const recoBooks: BookData[] = await response.json();
+
+  return (
+    <section>
+      <h3>지금 추천하는 도서</h3>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
+      <RecoBooks></RecoBooks>
+      <AllBooks></AllBooks>
     </div>
   );
 }
--- book-app-router/src/app/(with-searchbar)/search/page.tsx
+++ book-app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,22 +1,18 @@
-import BookItem from "@/components/book-item";
-import style from "../page.module.css";
 import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-export default function Home() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
