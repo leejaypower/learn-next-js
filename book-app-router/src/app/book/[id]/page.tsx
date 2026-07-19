c1caf6a1e9e273f5f5ccc68881d5f5d4490c0cdb
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2026-07-20 00:46:51 +0900

    서버 액션 1

--- book-app-router/src/app/book/[id]/page.tsx
+++ book-app-router/src/app/book/[id]/page.tsx
@@ -9,18 +9,8 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }]; // 문자열로만 명시해야함
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  // await을 하는 이유는? 쿼리 스트링 값은 런타임에 정해지기 때문에 동적 처리를 해야한다.
-  // promise로 주면 컴포넌트가 await params를 호출하는 순간을 next가 포착할 수 있다.
-  // = params를 실제로 읽기 전까지의 부분을 미리 렌더링하고, 읽는 순간부터 동적으로 전환같은 최적화가 가능해짐.
-  const { id } = await params;
-  // 즉 awiat은 이 값은 요청 시점에 결정되는 값이니 지금부터 동적 렌더링이 필요하다는 신호를 next에 주는 장치
-
-  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`;
+async function BookDetail({ bookId }: { bookId: string }) {
+  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`;
   const response = await fetch(url);
   // 동적 경로를 갖는 페이지에 한해 예외적으로 generateStaticParams가 있으면 static 페이지로 설정된다. (만약 여기에 coockies(), headers()같은 동적 api를 안쓴다면)
 
@@ -36,7 +26,7 @@ export default async function Page({
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -49,6 +39,45 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server"; // 서버 액션: 간결하고 편리하게 서버측에서 수행되는 동작을 정의할 수 있다.
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+
+    console.log(content, author);
+  }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰 내용"></input>
+        <input name="author" placeholder="작성자"></input>
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  // await을 하는 이유는? 쿼리 스트링 값은 런타임에 정해지기 때문에 동적 처리를 해야한다.
+  // promise로 주면 컴포넌트가 await params를 호출하는 순간을 next가 포착할 수 있다.
+  // = params를 실제로 읽기 전까지의 부분을 미리 렌더링하고, 읽는 순간부터 동적으로 전환같은 최적화가 가능해짐.
+  const { id } = await params;
+  // 즉 awiat은 이 값은 요청 시점에 결정되는 값이니 지금부터 동적 렌더링이 필요하다는 신호를 next에 주는 장치
+
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={id}></BookDetail>
+      <ReviewEditor></ReviewEditor>
     </div>
   );
 }
